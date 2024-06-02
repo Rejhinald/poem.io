784ae7273e29dfd686608c5a5dabfd60e458fda6
Arwin <arwinmiclat@gmail.com>
2024-06-03 01:32:33 +0800

    test 4

--- web/app/chat/page.tsx
+++ web/app/chat/page.tsx
@@ -17,7 +17,7 @@ export default function PoemGenerator() {
   const [userMessage, setUserMessage] = useState<string>("");
   const [messages, setMessages] = useState<Array<{ type: 'user' | 'poem', text: string, time: string }>>([]);
 
-//test 4
+//test 45
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
     if (storedToken) {
